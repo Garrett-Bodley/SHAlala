a6d805dac1a588b513af0749e510df53d175c907
Garrett Bodley <garrett.bodley@gmail.com>
2024-03-29 02:01:54 -0400

    Add various modes. Set volume lower.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-NOTE_MAPPING = {
+IONIAN = {
   0: "C4",
   1: "D4",
   2: "E4",
@@ -17,8 +17,125 @@ NOTE_MAPPING = {
   f: "D6",
 };
 
+DORIAN = {
+  0: "C4",
+  1: "D4",
+  2: "Eb4",
+  3: "F4",
+  4: "G4",
+  5: "A4",
+  6: "Bb4",
+  7: "C5",
+  8: "D5",
+  9: "Eb5",
+  a: "F5",
+  b: "G5",
+  c: "A5",
+  d: "Bb5",
+  e: "C6",
+  f: "D6",
+}
+
+PHRYGIAN = {
+  0: "C4",
+  1: "Db4",
+  2: "Eb4",
+  3: "F4",
+  4: "G4",
+  5: "Ab4",
+  6: "Bb4",
+  7: "C5",
+  8: "Db5",
+  9: "Eb5",
+  a: "F5",
+  b: "G5",
+  c: "Ab5",
+  d: "Bb5",
+  e: "C6",
+  f: "D6",
+}
+
+LYDIAN = {
+  0: "C4",
+  1: "D4",
+  2: "E4",
+  3: "F#4",
+  4: "G4",
+  5: "A4",
+  6: "B4",
+  7: "C5",
+  8: "D5",
+  9: "E5",
+  a: "F#5",
+  b: "G5",
+  c: "A5",
+  d: "B5",
+  e: "C6",
+  f: "D6",
+}
+
+MIXOLYDIAN = {
+  0: "C4",
+  1: "D4",
+  2: "E4",
+  3: "F4",
+  4: "G4",
+  5: "A4",
+  6: "Bb4",
+  7: "C5",
+  8: "D5",
+  9: "E5",
+  a: "F5",
+  b: "G5",
+  c: "A5",
+  d: "Bb5",
+  e: "C6",
+  f: "D6",
+}
+
+AEOLIAN = {
+  0: "C4",
+  1: "D4",
+  2: "Eb4",
+  3: "F4",
+  4: "G4",
+  5: "Ab4",
+  6: "Bb4",
+  7: "C5",
+  8: "D5",
+  9: "Eb5",
+  a: "F5",
+  b: "G5",
+  c: "Ab5",
+  d: "Bb5",
+  e: "C6",
+  f: "D6",
+}
+
+LOCRIAN = {
+  0: "C4",
+  1: "Db4",
+  2: "Eb4",
+  3: "F4",
+  4: "Gb4",
+  5: "Ab4",
+  6: "Bb4",
+  7: "C5",
+  8: "Db5",
+  9: "Eb5",
+  a: "F5",
+  b: "Gb5",
+  c: "Ab5",
+  d: "Bb5",
+  e: "C6",
+  f: "Db6",
+}
+
+MODES = [IONIAN, DORIAN, PHRYGIAN, LYDIAN, MIXOLYDIAN, AEOLIAN, LOCRIAN]
+
 let toneStart = false;
 let synth;
+let drone;
 
 document.addEventListener("DOMContentLoaded", (_e) => {
   document.getElementById("form").addEventListener("submit", handleOnSubmit);
@@ -30,12 +147,13 @@ document.addEventListener("DOMContentLoaded", (_e) => {
 async function initTone() {
   toneStart = true;
   await Tone.start();
+  Tone.Destination.volume.value = parseFloat(-20);
   Tone.Transport.bpm.value = 120;
 }
 
 async function handleRangeOnInput(e) {
   e.preventDefault();
-  if (toneStart == false) initTone();
+  if (toneStart == false) await initTone();
 
   let newRangeValue = e.target.value;
   document.getElementById("range-value").innerText = newRangeValue;
@@ -46,7 +164,7 @@ async function handleRangeOnInput(e) {
 
 async function handleOnSubmit(e) {
   e.preventDefault();
-  if (toneStart == false) initTone();
+  if (toneStart == false) await initTone();
 
   const inputText = document.getElementById("form-input").value;
   hash = await computeSHA1(inputText);
@@ -77,37 +195,39 @@ function hashToChords(hash) {
       hash
         .slice(i, i + 8)
         .split("")
-        .map((char) => NOTE_MAPPING[char])
+        .map((char) => MODES[Math.floor(Math.random() * MODES.length)][char])
     );
   }
   return chords;
 }
 
+function hashToNotes(hash) {
+  return hash.split("").map((char) => LYDIAN[char])
+  // return hash.split("").map((char) => MODES[Math.floor(Math.random() * MODES.length)][char])
+}
+
 function playNotes(hash) {
-  const chords = hashToChords(hash);
-  console.log(chords);
+  const notes = hashToNotes(hash);
+  console.log(notes);
 
-  console.log({ chords });
+  console.log({ notes });
 
-  if (synth) {
-    synth.dispose();
-  }
+  if (synth) synth.dispose();
+  if (drone) drone.dispose()
+
   synth = new Tone.PolySynth(Tone.Synth).toDestination();
+  drone = new Tone.PolySynth(Tone.Synth).toDestination();
   Tone.Transport.clear(0)
-  // events = chords.map(chord => {
-  //   new Tone.Event(time => {
-  //     synth.triggerAttackRelease(chord, '4n', time)
-  //   })
-  // })
 
   const sequence = new Tone.Sequence(
-    (time, chord) => {
-      // 'chord' is an element of the 'chords' array
-      synth.triggerAttackRelease(chord, "1n", time);
+    (time, note) => {
+      // 'note' is an element of the 'notes' array
+      synth.triggerAttackRelease(note, "8t", time);
     },
-    chords, // The array of chords to be played by the sequence
-    "1m" // The interval at which each chord is played ("1m" = once per measure)
+    notes, // The array of chords to be played by the sequence
+    "8t" // The interval at which each chord is played ("1m" = once per measure)
   );
+  drone.triggerAttack(['C2', 'C3'], Tone.now())
 
   sequence.start();
   Tone.Transport.start();
